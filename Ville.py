0b8415a968abbbb8c0346ea70ea251b6b350ca22
DarrenNA <darrenndouongo@gmail.com>
2023-10-26 13:16:09 +0200

    Début du Template du fonctionnement du changement de ville et des magasins

---
+++ Ville.py
@@ -0,0 +1,24 @@
+Class Magasin:
+    def __init__(self):
+        self.magasin
+
+
+Class Ville:
+    def __init__(self,nom,queteDeVille,evenementDeVille ,#evement,#teleporteur ):
+        self.nom = nom
+        self.region = region
+        self.magasin = magasin()
+        self.quete = quete
+
+        #ajout du constructeur d'une classe evenement
+        #un evenement peut etre une quête ou une rencontre qui pourrait engendrer la teleportation (pas que), tout comme l'entrée d'un magasin ou d'éventuel autre évenement
+
+
+
+
+        #ajout du construceur d'une classe teleporteur qui serait une classe à part et chaque ville aurait un teleporteur
+        # ( A voir comment les joueurs se deplaceront dans le jeu)
+
+
+
+
